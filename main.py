d45642888ff675c36ba35acd7908fe718106efad
VivaceJamiel <jimpoy2@gmail.com>
2022-07-23 01:11:56 -0500

    added level

---
+++ main.py
@@ -0,0 +1,24 @@
+import pygame, sys
+from settings import *
+from level import Level
+
+class Game:
+    def __init__(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
+        pygame.display.set_caption('Masks')
+        self.clock = pygame.time.Clock() 
+
+    def run(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+
+            self.screen.fill('black')
+            pygame.display.update()
+            self.clock.tick(FPS)
+
+if __name__ == '__main__':
+    game = Game()
+    game.run()
